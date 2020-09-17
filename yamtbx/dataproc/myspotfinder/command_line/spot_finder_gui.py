48da421c1e9775385ab2c614ab5ba5fdc4e1e412
root <root@bl41bss.bl41in.spring8.or.jp>
2020-09-17 15:17:38 +0900

    fix sqlite3 for ubuntu20.04

--- yamtbx/dataproc/myspotfinder/command_line/spot_finder_gui.py
+++ yamtbx/dataproc/myspotfinder/command_line/spot_finder_gui.py
@@ -5,7 +5,7 @@ import os
 import math
 import tempfile
 import getpass
-from sqlite3 import dbapi2 as sqlite
+from sqlite3 import dbapi2 as sqlite3
 import matplotlib
 matplotlib.interactive( True )
 matplotlib.use( 'WXAgg' )
