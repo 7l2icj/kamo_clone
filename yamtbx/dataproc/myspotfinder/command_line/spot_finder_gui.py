26798d5969e4944575fcb9e1e43ffac172813eb0
root <root@kuri01.spring8.or.jp>
2020-09-17 15:12:05 +0900

    fix for ubuntu20.04

--- yamtbx/dataproc/myspotfinder/command_line/spot_finder_gui.py
+++ yamtbx/dataproc/myspotfinder/command_line/spot_finder_gui.py
@@ -5,7 +5,7 @@ import os
 import math
 import tempfile
 import getpass
-import pysqlite2.dbapi2 as sqlite3
+from sqlite3 import dbapi2 as sqlite
 import matplotlib
 matplotlib.interactive( True )
 matplotlib.use( 'WXAgg' )
